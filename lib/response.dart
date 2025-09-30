c93ef40271a5d7c86edd6585e7ccdf3e97bc1a93
Ola Eklund <ola.eklund@live.se>
2025-09-30 21:17:22 +0200

    Fixing some bugs and improving the popup for images

--- lib/response.dart
+++ lib/response.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'data.dart';
 import 'dart:math';
 
@@ -8,7 +9,13 @@ class ResponseWidget extends StatefulWidget {
   final Set<String> flags;
   final CardList cardList;
 
-  const ResponseWidget({super.key, required this.response, required this.landscapeCards, required this.flags, required this.cardList});
+  const ResponseWidget({
+    super.key,
+    required this.response,
+    required this.landscapeCards,
+    required this.flags,
+    required this.cardList,
+  });
 
   @override
   State<ResponseWidget> createState() => _ResponseWidgetState();
@@ -16,74 +23,99 @@ class ResponseWidget extends StatefulWidget {
 
 class _ResponseWidgetState extends State<ResponseWidget> {
   final TextEditingController _controller = TextEditingController();
-  final CarouselController _carouselController = CarouselController();
   int _startingPlayer = 0;
 
   void _showCardImage(BuildContext context, MyCard card) {
+    card.parseSubCards(widget.cardList);
+    final List<MyCard> allCards = [card, ...?card.subCards];
+
     showDialog(
       context: context,
       builder: (BuildContext context) {
         card.parseSubCards(widget.cardList);
+        int currentIndex = 0;
         return AlertDialog(
           title: Text(card.name),
+          contentPadding: EdgeInsets.zero,
           content: SizedBox(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
-            child: CarouselView.weighted(
-              controller: _carouselController,
-              itemSnapping: true,
-              flexWeights: const <int>[1, 7, 1],
-              children: [
-                FutureBuilder(
-                  future: card.getImageUrl(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(child: CircularProgressIndicator());
-                    } else if (snapshot.hasError) {
-                      return Text('Error loading image, could not fetch from the wiki');
-                    } else if (!snapshot.hasData || snapshot.data == null) {
-                      return Text('No image available');
-                    }
-                    final String imageUrl = snapshot.data!;
-                    return FutureBuilder(
-                      future: precacheImage(NetworkImage(imageUrl), context),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState == ConnectionState.done) {
-                          return Image.network(imageUrl);
-                        } else {
-                          return Center(child: CircularProgressIndicator());
-                        }
-                      },
-                    );
-                  }
-                ),
-                for (MyCard subCard in card.subCards!)
-                  FutureBuilder(
-                    future: subCard.getImageUrl(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return Center(child: CircularProgressIndicator());
-                      } else if (snapshot.hasError) {
-                        return Text('Error loading image, could not fetch from the wiki');
-                      } else if (!snapshot.hasData || snapshot.data == null) {
-                        return Text('No image available');
-                      }
-                      final String imageUrl = snapshot.data!;
-                      return FutureBuilder(
-                        future: precacheImage(NetworkImage(imageUrl), context),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState == ConnectionState.done) {
-                            return Image.network(imageUrl);
-                          } else {
-                            return Center(child: CircularProgressIndicator());
-                          }
+            child: StatefulBuilder(
+              builder: (context, setState) {
+                return Stack(
+                  children: [
+                    CarouselSlider.builder(
+                      itemCount: allCards.length,
+                      options: CarouselOptions(
+                        enlargeCenterPage: true,
+                        enableInfiniteScroll: false,
+                        viewportFraction: 1.0,
+                        height: MediaQuery.of(context).size.height,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            currentIndex = index;
+                          });
                         },
-                      );
-                    }
-                  ),
-                ]
-              ),
+                      ),
+                      itemBuilder: (context, index, realIndex) {
+                        return FutureBuilder(
+                          future: allCards[index].getImageUrl(),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return Center(child: CircularProgressIndicator());
+                            } else if (snapshot.hasError) {
+                              return Text(
+                                'Error loading image, could not fetch from the wiki',
+                              );
+                            } else if (!snapshot.hasData ||
+                                snapshot.data == null) {
+                              return Text('No image available');
+                            }
+                            final String imageUrl = snapshot.data!;
+                            return InteractiveViewer(
+                              child: Image.network(
+                                imageUrl,
+                                fit: BoxFit.contain,
+                                loadingBuilder: (ctx, child, progress) {
+                                  if (progress == null) return child;
+                                  return const Center(
+                                    child: CircularProgressIndicator(),
+                                  );
+                                },
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                    Positioned(
+                      bottom: 1,
+                      right: 0,
+                      left: 0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(allCards.length, (index) {
+                          return Container(
+                            height: 10,
+                            width: 10,
+                            margin: const EdgeInsets.symmetric(horizontal: 4),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color:
+                                  currentIndex == index
+                                      ? Colors.blueAccent
+                                      : Colors.grey,
+                            ),
+                          );
+                        }),
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
+          ),
           actions: [
             TextButton(
               child: const Text('Close'),
@@ -100,12 +132,15 @@ class _ResponseWidgetState extends State<ResponseWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Response'),
-      ),
+      appBar: AppBar(title: const Text('Response')),
       body: ListView(
         children: [
-          Center(child: Text("Cards to be used:", style: Theme.of(context).textTheme.titleLarge)),
+          Center(
+            child: Text(
+              "Cards to be used:",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
           Divider(),
           ...widget.response.map((card) {
             return Padding(
@@ -115,20 +150,20 @@ class _ResponseWidgetState extends State<ResponseWidget> {
                 child: Row(
                   children: [
                     Image(
-                      image: AssetImage('assets/images/${card.expansion.replaceAll(" ", "_")}_icon.png'),
+                      image: AssetImage(
+                        'assets/images/${card.expansion.replaceAll(" ", "_")}_icon.png',
+                      ),
                       width: 24,
                       height: 24,
                     ),
-                    SizedBox(
-                      width: 5
-                    ),
+                    SizedBox(width: 5),
                     Expanded(child: Text(card.expansion)),
-                    for (AssetImage image in card.getCostImages(widget.cardList))
+                    for (AssetImage image in card.getCostImages(
+                      widget.cardList,
+                    ))
                       Image(image: image, width: 24, height: 24),
-                    SizedBox(
-                      width: 5
-                    ),
-                    Expanded(child: Text(card.name))
+                    SizedBox(width: 5),
+                    Expanded(child: Text(card.name)),
                   ],
                 ),
               ),
@@ -136,7 +171,12 @@ class _ResponseWidgetState extends State<ResponseWidget> {
           }),
           Divider(),
           if (widget.landscapeCards.isNotEmpty)
-            Center(child: Text("Landscape cards to use:", style: Theme.of(context).textTheme.titleLarge)),
+            Center(
+              child: Text(
+                "Landscape cards to use:",
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
           ...widget.landscapeCards.map((card) {
             return Padding(
               padding: const EdgeInsets.only(top: 4.0),
@@ -145,75 +185,91 @@ class _ResponseWidgetState extends State<ResponseWidget> {
                 child: Row(
                   children: [
                     Image(
-                      image: AssetImage('assets/images/${card.expansion.replaceAll(" ", "_")}_icon.png'),
+                      image: AssetImage(
+                        'assets/images/${card.expansion.replaceAll(" ", "_")}_icon.png',
+                      ),
                       width: 24,
                       height: 24,
                     ),
-                    SizedBox(
-                      width: 5
-                    ),
+                    SizedBox(width: 5),
                     Expanded(child: Text(card.expansion)),
-                    for (AssetImage image in card.getCostImages(widget.cardList))
+                    for (AssetImage image in card.getCostImages(
+                      widget.cardList,
+                    ))
                       Image(image: image, width: 24, height: 24),
-                    SizedBox(
-                      width: 5
-                    ),
-                    Expanded(child: Text(card.name))
+                    SizedBox(width: 5),
+                    Expanded(child: Text(card.name)),
                   ],
                 ),
               ),
             );
           }),
           Divider(),
-          Center(child: Text("Setup to do:", style: Theme.of(context).textTheme.titleLarge)),
-          for (MyCard card in widget.response.where((MyCard card) => card.setup != null && card.setup!.isNotEmpty)) 
+          Center(
+            child: Text(
+              "Setup to do:",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          for (MyCard card in widget.response.where(
+            (MyCard card) => card.setup != null && card.setup!.isNotEmpty,
+          ))
             Card(
               child: Column(
                 children: [
-                  Text(card.name, style: Theme.of(context).textTheme.titleMedium,),
+                  Text(
+                    card.name,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
                   Text(card.setup!),
                 ],
               ),
             ),
-          for (MyCard card in widget.landscapeCards.where((MyCard card) => card.setup != null && card.setup!.isNotEmpty)) 
+          for (MyCard card in widget.landscapeCards.where(
+            (MyCard card) => card.setup != null && card.setup!.isNotEmpty,
+          ))
             Card(
               child: Column(
                 children: [
-                  Text(card.name, style: Theme.of(context).textTheme.titleMedium,),
+                  Text(
+                    card.name,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
                   Text(card.setup!),
                 ],
               ),
             ),
-          Center(child: Text("You will need:", style: Theme.of(context).textTheme.titleMedium)),
+          Center(
+            child: Text(
+              "You will need:",
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
           Padding(
-            padding: const EdgeInsets.only(left:20.0), 
+            padding: const EdgeInsets.only(left: 20.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                if (widget.flags.contains("reserve"))
-                  Text("Tavern Mat"),
-                if (widget.flags.contains("debt"))
-                  Text("Debt counters"),
+                if (widget.flags.contains("reserve")) Text("Tavern Mat"),
+                if (widget.flags.contains("debt")) Text("Debt counters"),
                 if (widget.flags.contains("VPtokens"))
                   Text("Victory Point tokens"),
-                if (widget.flags.contains("ruins"))
-                  Text("Ruins cards"),
-                if (widget.flags.contains("boons"))
-                  Text("Boons"),
-                if (widget.flags.contains("hexes"))
-                  Text("Hexes"),
-                if (widget.flags.contains("spirits"))
-                  Text("Spirit cards"),
-                if (widget.flags.contains("spoils"))
-                  Text("Spoils cards"),
-                if (widget.flags.contains("wish"))
-                  Text("Wish cards"),
-                if (widget.flags.contains("potion"))
-                  Text("Potion cards"),
+                if (widget.flags.contains("ruins")) Text("Ruins cards"),
+                if (widget.flags.contains("boons")) Text("Boons"),
+                if (widget.flags.contains("hexes")) Text("Hexes"),
+                if (widget.flags.contains("spirits")) Text("Spirit cards"),
+                if (widget.flags.contains("spoils")) Text("Spoils cards"),
+                if (widget.flags.contains("wish")) Text("Wish cards"),
+                if (widget.flags.contains("potion")) Text("Potion cards"),
                 if (widget.flags.contains("omen"))
-                  Text("Use a Prophecy card. Put an amount of Sun tokens on it dependent on the number of players: 5 for 2 players, 8 for 3 players, 10 for 4 players, 12 for 5 players and 13 for 6 players."),
+                  Text(
+                    "Use a Prophecy card. Put an amount of Sun tokens on it dependent on the number of players: 5 for 2 players, 8 for 3 players, 10 for 4 players, 12 for 5 players and 13 for 6 players.",
+                  ),
                 if (widget.flags.contains("shelters"))
-                  Text("You should play with Shelters.", style: Theme.of(context).textTheme.titleMedium),
+                  Text(
+                    "You should play with Shelters.",
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
               ],
             ),
           ),
@@ -226,34 +282,39 @@ class _ResponseWidgetState extends State<ResponseWidget> {
           ),
           SizedBox(height: 10),
           Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      labelText: 'Enter number of players',
-                      border: OutlineInputBorder(),
-                    ),
-                    keyboardType: TextInputType.number,
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    labelText: 'Enter number of players',
+                    border: OutlineInputBorder(),
                   ),
+                  keyboardType: TextInputType.number,
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      int? inputNumber = int.tryParse(_controller.text);
-                      if (inputNumber == null || inputNumber < 1) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Please enter a valid number of players')),
-                        );
-                        return;
-                      }
-                      Random random = Random();
-                      int randomNumber = random.nextInt(inputNumber) + 1;
-                      _startingPlayer = randomNumber;
-                    });
-                  }, 
-                  child: Text('Randomize'))
-              ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    int? inputNumber = int.tryParse(_controller.text);
+                    if (inputNumber == null || inputNumber < 1) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                            'Please enter a valid number of players',
+                          ),
+                        ),
+                      );
+                      return;
+                    }
+                    Random random = Random();
+                    int randomNumber = random.nextInt(inputNumber) + 1;
+                    _startingPlayer = randomNumber;
+                  });
+                },
+                child: Text('Randomize'),
+              ),
+            ],
           ),
           if (_startingPlayer > 0)
             Padding(
